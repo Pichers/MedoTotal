18ad5ea3ef3957f0348cd0877014f037c9d1d63b
Antonio Almeida <antoniorafaelalmeida@hotmail.com>
2023-10-09 02:50:20 +0100

    MedoTotal.py changes, actions done, missing display correction

--- MedoTotal.py
+++ MedoTotal.py
@@ -1,4 +1,5 @@
 from searchPlus import *
+import utils
 
 parametros="T=26\nM=6\nP=10"
 linha1= "= = = = = = = = = =\n"
@@ -31,6 +32,7 @@ class MedoTotal(Problem):
         pastilhas = []
         
         for y in range(len(self.grid) - 1): 
+            self.grid[y] = utils.removeall(" ", self.grid[y])
             for x in range(len(self.grid[0]) - 1):
 
                 c = self.grid[y][x]
@@ -45,7 +47,7 @@ class MedoTotal(Problem):
                             pastilhas.append((x,y))
                     #pos do pacman
                     elif c == "@":
-                        pos = x,y
+                        pos = (x,y)
                     elif c == "F":
                         self.f = (x,y)
                     
@@ -63,20 +65,41 @@ class MedoTotal(Problem):
 
         pastDistances = MedoTotal.dists(p, past)
 
+        actions = []
+
         #instant cut
         if(len(past) == 0 and t > m):
             return []
         elif(min(pastDistances) > m):
+            print(min(pastDistances))
+            print("aa")
             return []
-        elif(min(pastDistances) + self.p * len(past)):
+        elif(min(pastDistances) + self.p * len(past) < t):
             return []
         
-        #actions (2 for each move because of spaces in the grid)
-        N = (0,-2)
-        S = (0,2)
-        E = (-2,0)
-        W = (2,0)
+        #actions (1 para N/S e 2 for each move because of spaces in the grid)
+        N = (0,-1)
+        W = (-1,0)
+        E = (1,0)
+        S = (0,1)
         
+        directions = [N, W, E, S]
+
+        for i in range(4):
+            d = directions[i]
+
+            #MAKE THIS A SWITCH CASE!!!
+            if(MedoTotal.canMove(self, state, d)):
+                if(d == N):
+                    actions.append("N")
+                elif(d == S):
+                    actions.append("S")
+                elif(d == E):
+                    actions.append("E")
+                elif(d == W):
+                    actions.append("W")
+        
+        return actions
 
         
     def result(self, state, action):
@@ -95,23 +118,29 @@ class MedoTotal(Problem):
     
     def display(self, state):
         s = ""
-        pGrid = self.grid
-        for y in range(len(self.grid)):
-            
-            for x in range(len(self.grid[0])):
-                if (x,y) in state[1]:
-                    pGrid[y] = pGrid[y][:x] + "*" + pGrid[y][x + 1:]
-                elif ((x,y) == (state[0][0], state[0][1])):
-                    pGrid[y] = pGrid[y][:x] + "@" + pGrid[y][x + 1:]
-                elif (x,y) == self.f:
-                    pGrid[y] = pGrid[y][:x] + "F" + pGrid[y][x + 1:]
+        pGrid = list(self.grid)  # Make a copy of the grid
 
-            s += self.grid[y] + "\n"
+        for y in range(len(self.grid)):  # Iterate over all rows
+            for x in range(len(self.grid[0])):  # Iterate over all columns
+                if (x, y) in state[1]:
+                    # Places the pastilhas
+                    pGrid[y] = pGrid[y][:x] + "*" + pGrid[y][x + 1:]
+                elif (x, y) == (state[0][0], state[0][1]):
+                    # Places the pacman
+                    pGrid[y] = pGrid[y][:x] + "@" + pGrid[y][x + 1:]
+                elif (x, y) == self.f:
+                    # Places the ghost
+                    pGrid[y] = pGrid[y][:x] + "F" + pGrid[y][x + 1:]
+                    
+        #after each character in a string, add " "
+
+        # Join the modified lines with spaces to form the final string
+        s = "\n".join(pGrid)
         return s
     
     @staticmethod
     def snakeDistance(a,b):
-        return abs(a[0] - b[0]) + abs(a[1] - b[1])
+        return abs((a[0] - b[0])) + abs(a[1] - b[1])
     
     @staticmethod
     def dists(x, ys):
@@ -121,5 +150,15 @@ class MedoTotal(Problem):
             d.append(MedoTotal.snakeDistance(x, ys[i]))
 
         return d
+    
+    @staticmethod
+    def canMove(self, state, d):
+        p = state[0]
 
+        newPos = (p[0] + d[0], p[1] + d[1])
 
+        c = self.grid[newPos[1]][newPos[0]]
+
+        canMove = (c != "=") and (newPos != self.f) 
+
+        return canMove
